ce1bd1bee387e857bc3dce6cb3e4aa79329aca35
Jeff <Jeff>
2017-03-05 17:04:45 -0500

    initial files/folders setup (not complete)

---
+++ server.js
@@ -0,0 +1,13 @@
+// REQUIRED NPM PACKAGES
+var express = require("express");
+var path = require("path");
+var logger = require("morgan");
+var cookieParser = require("cookie-parser");
+var bodyParser = require("body-parser");
+var session = require("express-session");
+var methodOverride = require("method-override");
+
+// CONTROLLERS
+// not made
+
+var app = express();
