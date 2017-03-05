6c540bee5cc685166d878dceb95114ba2b23e2dc
Jeff <Jeff>
2017-03-05 17:39:24 -0500

    Set up handlebars in server.js

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var cookieParser = require("cookie-parser");
 var bodyParser = require("body-parser");
 var session = require("express-session");
 var methodOverride = require("method-override");
+var exphbs = require("express-handlebars");
 
 // CONTROLLERS
 // not made
@@ -18,4 +19,15 @@ app.use(methodOverride("_method"));
 
 // Allow our app to use sessions and use of cookies
 // No time out set
-app.use(session({ secret: "app" }))
+app.use(session({ secret: "app" }));
+app.use(cookieParser());
+
+// Setting up view path for handlebars
+app.set("views", path.join(__dirname, "views"));
+// Registers the given template engine callback as ext.
+// app.engine(ext, callback)
+app.engine("handlebars", exphbs({
+	defaultLayout: // not chosen yet
+}));
+// Setting the default engine extension to use when omitted.
+app.set("view engine", "handlebars");
